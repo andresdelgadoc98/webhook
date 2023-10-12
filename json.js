072fb171767d81014eed068f55171a0865bdf3b5
Andrés Delgad <andresdelgadoc98@gmail.com>
2023-10-11 20:12:31 -0600

    node modules

---
+++ json.js
@@ -0,0 +1,35 @@
+{
+    data: {
+      end_device_ids: {
+        device_id: 'eui-24e124128c140062',
+        application_ids: [Object],
+        dev_eui: '24E124128C140062',
+        join_eui: '24E124C0002A0001',
+        dev_addr: '260CC1EE'
+      },
+      correlation_ids: [
+        'as:up:01H5KE4Q5M2JD5YHWTFBR7R2Y7',
+        'gs:conn:01H5ARVV8FRN130VVFCVCGTEDM',
+        'gs:up:host:01H5ARVVCMQ3T5Y1FS0HQR608X',
+        'gs:uplink:01H5KE4PZ4902R414DJ2T4PAPC',
+        'ns:uplink:01H5KE4PZ6E3P4Z6GPXXZAD3C3',
+        'rpc:/ttn.lorawan.v3.GsNs/HandleUplink:01H5KE4PZ6SVGG2MSNQTXFNMF8',
+        'rpc:/ttn.lorawan.v3.NsAs/HandleUplink:01H5KE4Q5JM97FK67JEK6JPW6C'
+      ],
+      received_at: '2023-07-18T02:44:18.227100018Z',
+      uplink_message: {
+        session_key_id: 'AYkShgXPr1d96xw2yeZRdQ==',
+        f_port: 85,
+        f_cnt: 23709,
+        frm_payload: 'A2cxAQRoeAZlCAAIAAIABWoAAAd94QEIfUMFCXNpJw==',
+        decoded_payload: [Object],
+        rx_metadata: [Array],
+        settings: [Object],
+        received_at: '2023-07-18T02:44:18.022253668Z',
+        confirmed: true,
+        consumed_airtime: '0.092416s',
+        network_ids: [Object]
+      }
+    }
+  }
+  {
--- src/apiServices/applications/Controller.js
+++ src/apiServices/applications/Controller.js
@@ -1,8 +1,27 @@
-exports.sendVariables = async (req, res) => {
-    const data = req.body
+const { query } = require('../../Helpers/functions');
 
+exports.sendVariables = async (req, res) => {
+    const data = JSON.parse(req.body.data)
     try {
-        console.log({ data })
+        const device_id = data.identifiers[0].device_ids.device_id
+        const device = await query (`SELECT Devices.DevicesName, Devices.DeviceType,DevicesUsers.DevicesUserID FROM DevicesUsers JOIN Devices ON DevicesUsers.DevicesTypeID = Devices.DevicesID WHERE DevicesUsers.DevicesIdTTN = ?;`,[device_id])
+        const date = data.time
+        const dateObj = new Date(date);
+
+        if(device[0].DevicesName == 'Ambientales'){
+            const decoded_payload = data.data.uplink_message.decoded_payload
+       
+            const dataBulk = []
+
+            for (let i = 0; i < Devices.length; i++) {
+                    dataBulk.push([Devices[i].DevicesID, decoded_payload[Devices[i].DevicesName],dateObj.toISOString().slice(0, 19).replace('T', ' ')])
+             
+            }
+   
+         const insert = await query (`INSERT INTO SensorReadings (SensorID,Value,Timestamp) values ?`,[dataBulk])
+           
+        }
+
         res.status(200).json('')
 
     } catch (e) {
