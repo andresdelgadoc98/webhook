e1e5c518b8d39f764f7c42498426da4f4ab724a6
Andres Delgado​ <andresdelgadoc98@gmail.com>
2023-10-11 21:45:47 -0500

    controller ambientales finished

--- src/apiServices/applications/Controller.js
+++ src/apiServices/applications/Controller.js
@@ -1,27 +1,35 @@
 const { query } = require('../../Helpers/functions');
+const moment = require('moment-timezone');
 
 exports.sendVariables = async (req, res) => {
-    const data = JSON.parse(req.body.data)
-    try {
-        const device_id = data.identifiers[0].device_ids.device_id
-        const device = await query (`SELECT Devices.DevicesName, Devices.DeviceType,DevicesUsers.DevicesUserID FROM DevicesUsers JOIN Devices ON DevicesUsers.DevicesTypeID = Devices.DevicesID WHERE DevicesUsers.DevicesIdTTN = ?;`,[device_id])
-        const date = data.time
-        const dateObj = new Date(date);
 
-        if(device[0].DevicesName == 'Ambientales'){
-            const decoded_payload = data.data.uplink_message.decoded_payload
-       
-            const dataBulk = []
-
-            for (let i = 0; i < Devices.length; i++) {
-                    dataBulk.push([Devices[i].DevicesID, decoded_payload[Devices[i].DevicesName],dateObj.toISOString().slice(0, 19).replace('T', ' ')])
-             
-            }
+    const data = req.body
    
-         const insert = await query (`INSERT INTO SensorReadings (SensorID,Value,Timestamp) values ?`,[dataBulk])
-           
+    try {
+        const device_id = data.end_device_ids.device_id
+        const device = await query (`SELECT Devices.DevicesName, Devices.DeviceType,DevicesUsers.DevicesUserID FROM DevicesUsers JOIN Devices ON DevicesUsers.DevicesTypeID = Devices.DevicesID WHERE DevicesUsers.DevicesIdTTN = ?;`,[device_id])
+        const date = data.received_at
+        const dateObj = new Date(date);
+        console.log({dateObj})
+        if(device.length > 0){
+            if(device[0].DevicesName == 'Ambientales'){
+                const decoded_payload = data.uplink_message.decoded_payload
+                const Devices = await query (`SELECT * FROM Devices where PyhiscalDevice = 'Ambiental';`,[])
+                const mexicoTime = moment(dateObj).tz("America/Mexico_City").format('YYYY-MM-DD HH:mm:ss');
+    
+                const dataBulk = []
+    
+                for (let i = 0; i < Devices.length; i++) {
+                        dataBulk.push([Devices[i].DevicesID, decoded_payload[Devices[i].DevicesName],mexicoTime])
+                 
+                }
+       
+             const insert = await query (`INSERT INTO SensorReadings (SensorID,Value,Timestamp) values ?`,[dataBulk])
+               
+            }
+    
         }
-
+      
         res.status(200).json('')
 
     } catch (e) {
